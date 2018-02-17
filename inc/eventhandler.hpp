01715de8e1886b247f35d5173b88da785a352e1b
2kreate <daniel@2kreate.com>
2018-02-17 02:05:01 -0700

    Input handler functional, Moving forward with event queue.

--- inc/eventhandler.hpp
+++ inc/eventhandler.hpp
@@ -2,17 +2,76 @@
 
 #include <SDL2/SDL.h>
 #include <array>
-#include <iterator>
+#include <map>
+#include <vector>
+#include <iostream>
 
+#define DEBUG(e) std::cerr << e << std::endl;
 
 class EventHandler
 {
 private:
     bool initialized;
-    std::array<bool,285> keystate{};
+    std::array<bool,285> keyState{};
+    enum commands{
+		JUMP,
+		DUCK,
+		BACK,
+		FORWARD,
+		ACTION,
+		SPECIAL,
+		QUIT,
+		NUM_OF_COMMANDS
+	};
+	std::map<int,SDL_Keycode> keysToCommands; 
+	class Command {
+		public:
+		  virtual ~Command() {};
+		  virtual void execute() = 0;
+	};
+	class JumpCommand : public Command {
+		public:
+		  virtual void execute() { 
+			  DEBUG("Execute Jump");	
+			  //jump(); 
+			  return;
+		  }
+	};
+	class DuckCommand : public Command {
+		public:
+		  virtual void execute() { 
+			  //duck(); 
+		  }
+	};
+	class MoveCommand : public Command {
+		public:
+		  virtual void execute() { 
+			  //Need to have object pointer
+			  //Update object position 
+		  }
+	};
+	class ActionCommand : public Command {
+		public:
+		  virtual void execute() { 
+			  //action(); 
+		  }
+	};
+	class SpecialCommand : public Command {
+		public:
+		  virtual void execute() { 
+			  //special(); 
+		  }
+	};
+	std::vector<Command*> eventStack;
+	JumpCommand* jumpCommand = new JumpCommand;
+	MoveCommand* moveCommand = new MoveCommand;
+	ActionCommand* actionCommand = new ActionCommand;
+	SpecialCommand* specialCommand = new SpecialCommand;
+	DuckCommand* duckCommand = new DuckCommand;
 public:
     EventHandler();
     ~EventHandler();
     bool isInitialized() const;
-    int InputHandler(SDL_Event &event);
+    int inputHandler(SDL_Event &event);
+    void addEvent();
 };
--- src/eventhandler.cpp
+++ src/eventhandler.cpp
@@ -1,44 +1,90 @@
 #include <inc/eventhandler.hpp>
 #include <iostream>
 
+#define DEBUG(e) std::cerr << e << std::endl;
+
 //.cbutton for joystick
 
 EventHandler::EventHandler(){
 	initialized = true;
-	keystate.fill(const false);
+	keyState.fill(false);
+	keysToCommands[SDLK_UP]=JUMP;
+	keysToCommands[SDLK_DOWN]=DUCK;
+	keysToCommands[SDLK_LEFT]=BACK;
+	keysToCommands[SDLK_RIGHT]=FORWARD;
+	keysToCommands[SDLK_f]=ACTION;
+	keysToCommands[SDLK_d]=SPECIAL;
+	keysToCommands[SDLK_ESCAPE]=QUIT;
 }
 
 EventHandler::~EventHandler(){
 }
 
-
-
-int EventHandler::InputHandler(SDL_Event &event){
+int EventHandler::inputHandler(SDL_Event &event){
 	while(SDL_PollEvent(&event) != 0){
 		if (event.type == SDL_QUIT) return 1;
 		switch(event.type){
 			case SDL_KEYDOWN: 
-				std::cout << "Keypress detected. Scancode: "<<event.key.keysym.scancode<<" & array value: "<<keystate[event.key.keysym.scancode]<<std::endl;
-				if (keystate[event.key.keysym.scancode] == true){
+				DEBUG("Keypress detected. Scancode: "<<event.key.keysym.scancode<<" & array value: "<<keyState[event.key.keysym.scancode]);
+				if (keyState[event.key.keysym.scancode] == true){
 					return 0;
 				 }
-				switch (event.key.keysym.sym){
-					case SDLK_LEFT:
-						std::cout << "Left arrow"<<std::endl;
+				switch (keysToCommands[event.key.keysym.sym]){
+					case JUMP:
+						DEBUG("Jump");
+						keyState[event.key.keysym.scancode]=true;
+						eventStack.push_back(jumpCommand);
 						return 0;
-					case SDLK_RIGHT:
-						std::cout << "Right arrow"<<std::endl;
+					case DUCK:
+						keyState[event.key.keysym.scancode]=true;
+						eventStack.push_back(duckCommand);
+						DEBUG("Duck");
 						return 0;
-					case SDLK_UP:
-						std::cout << "Up arrow"<<std::endl;
+					case BACK:
+						keyState[event.key.keysym.scancode]=true;
+						eventStack.push_back(moveCommand);
+						DEBUG("Back");
 						return 0;
-					case SDLK_DOWN:
-						std::cout << "Down arrow"<<std::endl;
+					case FORWARD:
+						keyState[event.key.keysym.scancode]=true;
+						eventStack.push_back(moveCommand);
+						DEBUG("Forward");
+						return 0;
+					case ACTION:
+						DEBUG("Action");
+						keyState[event.key.keysym.scancode]=true;
+						eventStack.push_back(actionCommand);
+						return 0;
+					case SPECIAL:
+						DEBUG("Special");
+						keyState[event.key.keysym.scancode]=true;
+						eventStack.push_back(specialCommand);
+						return 0;
+					case QUIT:
+						DEBUG("Quit");
+						return 1;
+				}
+			case SDL_KEYUP: 
+				switch (keysToCommands[event.key.keysym.sym]){
+					case JUMP:
+						keyState[event.key.keysym.scancode]=false;
+						return 0;
+					case DUCK:
+						keyState[event.key.keysym.scancode]=false;
+						return 0;
+					case BACK:
+						keyState[event.key.keysym.scancode]=false;
+						return 0;
+					case FORWARD:
+						keyState[event.key.keysym.scancode]=false;
+						return 0;
+					case ACTION:
+						keyState[event.key.keysym.scancode]=false;
+						return 0;
+					case SPECIAL:
+						keyState[event.key.keysym.scancode]=false;
 						return 0;
 				}
-				return 0;
-			case SDL_KEYUP: 
-				return 0;
 		}
 	}
 }
