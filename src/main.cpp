831d0992e977f88d18c6be0a5e5c66c041eb861a
Tyler Jones <tdjones879@gmail.com>
2018-03-04 17:09:52 -0700

    Working example for falling square

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 #include "inc/eventhandler.hpp"
 #include "inc/displaymanager.hpp"
+#include "inc/physics/world.hpp"
+#include "inc/physics/common.hpp"
+#include "inc/physics/polygon.hpp"
 
 const int MIN_MILLISECONDS_PER_FRAME = 16;
 
@@ -19,16 +22,27 @@ int main(int argc, char **args)
         return 1;
     }
 
+    phy::World world(phy::Vec2(3, 5));
+    phy::BodySpec spec;
+    spec.bodyType = phy::BodyType::dynamicBody;
+    auto shape = phy::PolygonShape(0.5f);
+    shape.setBox(phy::Vec2(25, 25));
+    auto body = world.createBody(spec);
+    auto shape_ptr = body.lock()->addShape(shape);
+
     bool quit = false;
     SDL_Event e{};
     while (!quit) {
         const int start = SDL_GetTicks();
-        displayManager.displayPolygon();
+        displayManager.displayPolygon(body, shape_ptr);
         if (eventHandler.inputHandler(e) == 1)
             return 0;
         eventHandler.executeEvents();
-        const int end = SDL_GetTicks();
-        const int millisecondsThisFrame = end - start;
+
+        world.step();
+        std::cout << *body.lock();
+
+        const int millisecondsThisFrame = SDL_GetTicks() - start;
         if (millisecondsThisFrame < MIN_MILLISECONDS_PER_FRAME) {
             // If rendering faster than 60FPS, delay
             SDL_Delay(MIN_MILLISECONDS_PER_FRAME - millisecondsThisFrame);
