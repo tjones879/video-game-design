c0a990f0132ecc2d514a5352f14ec0962cefc10a
Tyler Jones <tdjones879@gmail.com>
2018-03-11 16:47:25 -0600

    Move aabb implementation to source file.

--- inc/physics/aabb.hpp
+++ inc/physics/aabb.hpp
@@ -12,23 +12,9 @@ public:
     AABB(Vec2f lower, Vec2f higher)
         : lowVertex(lower), highVertex(higher) {}
 
-    Vec2f getCenter() const
-    {
-        return 0.5f * (lowVertex + highVertex);
-    }
-
-    float getPerimeter() const
-    {
-        float x = highVertex.x - lowVertex.x;
-        float y = highVertex.y - lowVertex.y;
-        return 2.f * (x + y);
-    }
-
-    AABB combine(const AABB &b)
-    {
-        return AABB(minValues(lowVertex, b.lowVertex),
-                    maxValues(highVertex, b.highVertex));
-    }
+    Vec2f getCenter() const;
+    float getPerimeter() const;
+    AABB combine(const AABB &b);
 
     template <class T, class... Args>
     AABB combine(T first, Args&&... args)
@@ -38,22 +24,12 @@ public:
 
     /**
      * Determine if this AABB contains the given AABB.
+     *
+     * @return True if the other AABB is completely inside.
      */
-    bool contains(const AABB &other)
-    {
-        return lowVertex.below(other.lowVertex) && highVertex.above(other.highVertex);
-    }
+    bool contains(const AABB &other);
 };
 
-bool operator==(const AABB &a, const AABB& b)
-{
-    return (a.lowVertex == b.lowVertex) && (a.highVertex == b.highVertex);
-}
-
-std::ostream &operator<<(std::ostream &out, const AABB &aabb)
-{
-    out << "Lower: " << aabb.lowVertex
-        << "Higher: " << aabb.highVertex << std::endl;
-    return out;
-}
+bool operator==(const AABB &a, const AABB& b);
+std::ostream &operator<<(std::ostream &out, const AABB &aabb);
 } /* namespace phy */
---
+++ src/physics/aabb.cpp
@@ -0,0 +1,39 @@
+#include "inc/physics/aabb.hpp"
+
+namespace phy {
+Vec2f AABB::getCenter() const
+{
+    return 0.5f * (lowVertex + highVertex);
+}
+
+float AABB::getPerimeter() const
+{
+    float x = highVertex.x - lowVertex.x;
+    float y = highVertex.y - lowVertex.y;
+    return 2.f * (x + y);
+}
+
+AABB AABB::combine(const AABB &b)
+{
+    return AABB(minValues(lowVertex, b.lowVertex),
+                maxValues(highVertex, b.highVertex));
+}
+
+bool AABB::contains(const AABB &other)
+{
+    return lowVertex.below(other.lowVertex) && highVertex.above(other.highVertex);
+}
+
+bool operator==(const AABB &a, const AABB& b)
+{
+    return (a.lowVertex == b.lowVertex)
+        && (a.highVertex == b.highVertex);
+}
+
+std::ostream &operator<<(std::ostream &out, const AABB &aabb)
+{
+    out << "Lower: " << aabb.lowVertex
+        << "Higher: " << aabb.highVertex << std::endl;
+    return out;
+}
+} /* namespace phy */
